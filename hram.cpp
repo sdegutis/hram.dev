e63d153648cc9cc5a9f57a253fda06446f5ee21c
Steven Degutis <sbdegutis@gmail.com>
2025-06-19 13:47:36 -0500

    First

---
+++ hram.cpp
@@ -0,0 +1,39 @@
+﻿#include <fmt/core.h>
+
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <toml++/toml.hpp>
+
+using namespace std::literals;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+	fmt::print("Hello World!\n");
+
+	auto x = toml::parse("foo = 'bar'");
+	fmt::print("Hello World! {}\n", x.is_boolean());
+
+	SDL_Window* win = SDL_CreateWindow("hram", 320 * 2, 180 * 2, SDL_WINDOW_RESIZABLE);
+
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate) {
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* e) {
+	switch (e->type) {
+
+	case SDL_EVENT_QUIT:
+		return SDL_APP_SUCCESS;
+
+	}
+
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+	fmt::println("quittin");
+}
