a2e91b71375acb9f1590820023aa6bba976d53d4
Steven Degutis <sbdegutis@gmail.com>
2025-06-25 11:07:42 -0500

    Test.

--- src/main.c
+++ src/main.c
@@ -7,14 +7,48 @@
 #include <lua.h>
 #include <lauxlib.h>
 
-SDL_Window* win;
+SDL_Window* window;
+
+SDL_Rect srcrect = { .x = 0, .y = 0, .w = 320, .h = 180 };
+SDL_Rect destrect;
+int scale = 1;
+
+SDL_Surface* appsurf;
+SDL_Surface* winsurf;
+
+uint8_t rawdata[320 * 180 * 4];
+
+void resize() {
+	destrect = srcrect;
+	scale = 1;
+
+	int w, h;
+	auto winsize = SDL_GetWindowSize(window, &w, &h);
+
+	while (destrect.w + srcrect.w <= w && destrect.h + srcrect.h <= h) {
+		scale++;
+		destrect.w += srcrect.w;
+		destrect.h += srcrect.h;
+	}
+
+	destrect.x = w / 2 - destrect.w / 2;
+	destrect.y = h / 2 - destrect.h / 2;
+}
+
+void blit() {
+	SDL_BlitSurfaceUncheckedScaled(appsurf, &srcrect, winsurf, &destrect, SDL_SCALEMODE_NEAREST);
+	SDL_UpdateWindowSurface(window);
+}
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
 	lua_State* lib = luaL_newstate();
 	printf("%p ?\n", lib);
 
-	win = SDL_CreateWindow("H-RAM", 320 * 3, 180 * 3, SDL_WINDOW_RESIZABLE);
+	window = SDL_CreateWindow("H-RAM", 320 * 3, 180 * 3, SDL_WINDOW_RESIZABLE);
+	appsurf = SDL_CreateSurfaceFrom(320, 180, SDL_PIXELFORMAT_XRGB8888, rawdata, 320 * 4);
+	winsurf = SDL_GetWindowSurface(window);
+	resize();
 
 	return SDL_APP_CONTINUE;
 }
@@ -35,6 +69,14 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* e)
 
 	case SDL_EVENT_MOUSE_MOTION:
 		printf("%d, %d\n", (int)e->motion.x, (int)e->motion.y);
+		*((uint32_t*)rawdata + (SDL_rand(320 * 180))) = SDL_rand(0xffffff);
+		blit();
+		break;
+
+	case SDL_EVENT_WINDOW_RESIZED:
+		winsurf = SDL_GetWindowSurface(window);
+		resize();
+		blit();
 		break;
 
 	case SDL_EVENT_MOUSE_BUTTON_DOWN:
