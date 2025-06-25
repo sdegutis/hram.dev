eabbbb283ab95585341da4b2f319afb2d5293754
Steven Degutis <sbdegutis@gmail.com>
2025-06-25 09:31:28 -0500

    One file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,62 @@
 ﻿#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
 
-#include "app.hpp"
 #include <format>
 #include <vars.h>
+#include <print>
 
 #include <lua.hpp>
 #include <filesystem>
 
+class App
+{
+
+	SDL_Window* win;
+
+public:
+
+	App();
+
+	void iterate();
+	void mouseMoved(int32_t x, int32_t y);
+	void mouseButton(int button, bool down);
+
+};
+
+App::App()
+{
+	win = SDL_CreateWindow("H-RAM", 320 * 3, 180 * 3, SDL_WINDOW_RESIZABLE);
+}
+
+void App::iterate()
+{
+	auto ticks = SDL_GetTicks();
+}
+
+void App::mouseMoved(int32_t x, int32_t y)
+{
+	std::println("{},{}", x, y);
+}
+
+void App::mouseButton(int button, bool down)
+{
+	std::println("{},{}", button, down);
+}
+
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
 	auto lib = luaL_newstate();
-	printf("%p\n", lib);
+	printf("%p ?\n", lib);
 
-	std::filesystem::path userDir(SDL_GetPrefPath("90sdev", "hram"));
-	std::filesystem::path appDir(SDL_GetBasePath());
-	std::filesystem::path userBootWat = userDir / "boot.wat";
+	//std::filesystem::path userDir(SDL_GetPrefPath("90sdev", "hram"));
+	//std::filesystem::path appDir(SDL_GetBasePath());
+	//std::filesystem::path userBootWat = userDir / "boot.wat";
 
-	std::filesystem::copy_file(
-		appDir / "boot.wat",
-		userBootWat,
-		std::filesystem::copy_options::skip_existing);
+	//std::filesystem::copy_file(
+	//	appDir / "boot.wat",
+	//	userBootWat,
+	//	std::filesystem::copy_options::skip_existing);
 
 	auto app = new App();
 	*appstate = app;
