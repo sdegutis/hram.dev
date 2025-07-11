d54b88a03e6e66948357e7b20da35b7edb578991
Steven Degutis <sbdegutis@gmail.com>
2025-07-10 22:32:00 -0500

    util

--- hram/main.cpp
+++ hram/main.cpp
@@ -76,26 +76,6 @@ LRESULT CALLBACK WindowProc2(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 int diffw;
 int diffh;
 
-std::string getUserDir() {
-	PWSTR thepath;
-	SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, NULL, &thepath);
-	auto s = std::string(CW2A(thepath, CP_UTF8));
-	CoTaskMemFree(thepath);
-	return s;
-}
-
-std::string getExeName() {
-	TCHAR szFileName[MAX_PATH];
-	GetModuleFileName(NULL, szFileName, MAX_PATH);
-	return std::string(CW2A(szFileName, CP_UTF8));
-}
-
-void openConsole() {
-	AllocConsole();
-	freopen_s((FILE**)stdout, "CONOUT$", "w", stdout);
-	freopen_s((FILE**)stdin, "CONIN$", "r", stdin);
-}
-
 
 
 int luaopen_foo(lua_State* L) {
