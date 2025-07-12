096d0268f3311888fa9a32a81cd9a1626a26f8e8
Steven Degutis <sbdegutis@gmail.com>
2025-07-12 16:33:05 -0500

    forgot this

--- hram/main.cpp
+++ hram/main.cpp
@@ -11,9 +11,6 @@
 
 #include <string>
 
-#include <asmjit/asmjit.h>
-#include <asmtk/asmtk.h>
-
 #include "PixelShader.h"
 #include "VertexShader.h"
 #include <lua/lua.hpp>
@@ -84,63 +81,10 @@ Image s(4, 4);
 
 #include "util.h"
 
-using namespace asmjit;
-using namespace asmtk;
-
-static void dumpCode(const uint8_t* buf, size_t size) {
-	constexpr uint32_t kCharsPerLine = 39;
-	char hex[kCharsPerLine * 2 + 1];
-
-	size_t i = 0;
-	while (i < size) {
-		size_t j = 0;
-		size_t end = size - i < kCharsPerLine ? size - i : size_t(kCharsPerLine);
-
-		end += i;
-		while (i < end) {
-			uint8_t b0 = buf[i] >> 4;
-			uint8_t b1 = buf[i] & 15;
-
-			hex[j++] = b0 < 10 ? '0' + b0 : 'A' + b0 - 10;
-			hex[j++] = b1 < 10 ? '0' + b1 : 'A' + b1 - 10;
-			i++;
-		}
-
-		hex[j] = '\0';
-		puts(hex);
-	}
-}
-
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR pCmdLine, _In_ int nCmdShow) {
 
 	openConsole();
-
-	// Setup CodeHolder for X64.
-	Environment env(Arch::kX64);
-	CodeHolder code;
-	code.init(env);
-
-	// Attach x86::Assembler to `code`.
-	x86::Assembler a(&code);
-
-	// Create AsmParser that will emit to x86::Assembler.
-	AsmParser p(&a);
-
-	// Parse some assembly.
-	Error err = p.parse(
-		"mov rax, rbx\n"
-		"vaddpd zmm0, zmm1, [rax + 128]\n");
-
-	// Error handling (use asmjit::ErrorHandler for more robust error handling).
-	if (err) {
-		printf("ERROR: %08x (%s)\n", err, DebugUtils::errorAsString(err));
-		return 1;
-	}
-
-	// Now you can print the code, which is stored in the first section (.text).
-	CodeBuffer& buffer = code.sectionById(0)->buffer();
-	dumpCode(buffer.data(), buffer.size());
 
 
 	checkSubWindow();
