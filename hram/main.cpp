db3a3bf97da779e1bb601453f65a5b7ff9b7d303
Steven Degutis <sbdegutis@gmail.com>
2025-07-12 22:16:38 -0500

    box2d!

--- hram/main.cpp
+++ hram/main.cpp
@@ -85,6 +85,8 @@ Image s(4, 4);
 #include <stdio.h>
 #include <asmtk/asmtk.h>
 
+#include <box2d/box2d.h>
+
 using namespace asmjit;
 using namespace asmtk;
 
@@ -96,6 +98,35 @@ typedef int (*Func)(int);
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR pCmdLine, _In_ int nCmdShow) {
 
 	openConsole();
+
+	b2WorldDef worldDef = b2DefaultWorldDef();
+	worldDef.gravity = { 0.0f, -10.0f };
+	b2WorldId worldId = b2CreateWorld(&worldDef);
+	b2BodyDef groundBodyDef = b2DefaultBodyDef();
+	groundBodyDef.position = { 0.0f, -10.0f };
+	b2BodyId groundId = b2CreateBody(worldId, &groundBodyDef);
+	b2Polygon groundBox = b2MakeBox(50.0f, 10.0f);
+	b2ShapeDef groundShapeDef = b2DefaultShapeDef();
+	b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
+	b2BodyDef bodyDef = b2DefaultBodyDef();
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position = { 0.0f, 4.0f };
+	b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
+	b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
+	b2ShapeDef shapeDef = b2DefaultShapeDef();
+	shapeDef.density = 1.0f;
+	shapeDef.material.friction = 0.3f;
+	b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
+	float timeStep = 1.0f / 60.0f;
+	int subStepCount = 4;
+	for (int i = 0; i < 90; ++i)
+	{
+		b2World_Step(worldId, timeStep, subStepCount);
+		b2Vec2 position = b2Body_GetPosition(bodyId);
+		b2Rot rotation = b2Body_GetRotation(bodyId);
+		printf("%4.2f %4.2f %4.2f\n", position.x, position.y, b2Rot_GetAngle(rotation));
+	}
+
 
 	JitRuntime rt;
 
