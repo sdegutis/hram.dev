6ff36fb3739d021c4f68c8a033be00c2b12dc9cd
Steven Degutis <sbdegutis@gmail.com>
2025-07-17 22:46:42 -0500

    done!?

--- hram/foo.lua
+++ hram/foo.lua
@@ -9,23 +9,27 @@ require 'bar'
 local m = memory.malloc(3*3*4)
 for i = 0,3*3*4-1 do memory.set(m+i, 8, math.random(0xff)-1) end
 local img = image.create(m, 3, 3)
-memory.free(m)
+--memory.free(m)
+
+image.copy(nil, img, 0, 0)
 
 local m2 = memory.malloc(9*9*4)
 memory.fill(m2, 0, 9*9*4)
 local img2 = image.create(m2, 9, 9)
 memory.free(m2)
 
-
 image.copy(img2, img, 0, 0, 0, 0, 3, 3)
 image.copy(img2, img, 6, 0, 0, 0, 3, 3)
 image.copy(img2, img, 0, 6, 0, 0, 3, 3)
 image.copy(img2, img, 6, 6, 0, 0, 3, 3)
 
+image.delete(img)
+
 
 
 function mousemove(x, y)
-	image.copy(nil, img2, x, y)
+	--image.copy(nil, img2, x, y)
+	image.update(nil, x, y, 2, 2, m+4*4, 3*4)
 	print("mouse moved", x, y)
 end
 
--- hram/image.cpp
+++ hram/image.cpp
@@ -54,7 +54,6 @@ static int putimage(lua_State* L) {
 	auto dy = lua_tointeger(L, 4);
 
 	if (lua_gettop(L) == 4) {
-		printf("here\n");
 		devicecontext->CopySubresourceRegion(dst, 0, dx, dy, 0, src, 0, NULL);
 	}
 	else {
@@ -72,10 +71,33 @@ static int putimage(lua_State* L) {
 	return 0;
 }
 
+static int updimage(lua_State* L) {
+	auto dst = reinterpret_cast<ID3D11Texture2D*>(lua_tointeger(L, 1));
+	if (dst == nullptr) dst = screen->texture;
+
+	auto dx = lua_tointeger(L, 2);
+	auto dy = lua_tointeger(L, 3);
+	auto dw = lua_tointeger(L, 4);
+	auto dh = lua_tointeger(L, 5);
+
+	auto mem = reinterpret_cast<void*>(lua_tointeger(L, 6));
+
+	auto pw = lua_tointeger(L, 7);
+
+	D3D11_BOX box;
+	box.left = dx; box.right = dx + dw;
+	box.top = dy; box.bottom = dy + dh;
+	box.front = 0; box.back = 1;
+	devicecontext->UpdateSubresource(dst, 0, &box, mem, pw, 0);
+
+	return 0;
+}
+
 static const struct luaL_Reg imagelib[] = {
 	{"create", newimage},
 	{"delete", delimage},
-	{"copy", putimage},
+	{"copy",   putimage},
+	{"update", updimage},
 	{NULL, NULL}
 };
 
@@ -83,63 +105,3 @@ int luaopen_image(lua_State* L) {
 	luaL_newlib(L, imagelib);
 	return 1;
 }
-
-//	void pset(int x, int y, uint32_t c)
-//	{
-//		D3D11_BOX box;
-//		box.top = y;
-//		box.bottom = y + 1;
-//		box.left = x;
-//		box.right = x + 1;
-//		box.front = 0;
-//		box.back = 1;
-//		devicecontext->UpdateSubresource(texture, 0, &box, &c, 4, 4);
-//	}
-//
-//	void fillrect(int x, int y, int w, int h, int c)
-//	{
-//		auto buf = (uint32_t*)malloc(w * h * 4);
-//		if (buf == 0) throw std::exception("oom");
-//		for (int i = 0; i < w * h; i++) buf[i] = c;
-//
-//		D3D11_BOX box;
-//		box.top = y;
-//		box.bottom = y + h;
-//		box.left = x;
-//		box.right = x + w;
-//		box.front = 0;
-//		box.back = 1;
-//		devicecontext->UpdateSubresource(texture, 0, &box, buf, w * 4, w * h * 4);
-//
-//		free(buf);
-//	}
-//
-//
-//	void copyTo(Image& dst, int dx, int dy)
-//	{
-//		devicecontext->CopySubresourceRegion(dst.texture, 0, dx, dy, 0, texture, 0, NULL);
-//	}
-//
-//	void copyTo(Image& dst, int dx, int dy, int sx, int sy, int sw, int sh)
-//	{
-//		D3D11_BOX box;
-//		box.left = sx;
-//		box.right = sx + sw;
-//		box.top = sy;
-//		box.bottom = sy + sh;
-//		box.front = 0;
-//		box.back = 1;
-//		devicecontext->CopySubresourceRegion(dst.texture, 0, dx, dy, 0, texture, 0, &box);
-//	}
-//
-//
-//};
-//
-//	s.create(device, devicecontext);
-//
-//	s.fillrect(0, 0, 4, 4, 0xff9900);
-//	s.fillrect(1, 1, 2, 2, 0xff99ff);
-//
-//	s.copyTo(screen1, 10, 10);
-//	s.copyTo(screen1, 0, 0, 1, 1, 2, 2);
-//
