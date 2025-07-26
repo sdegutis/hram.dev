1ea564c0e2d94dffcbd308a6f5f11a14d874aeb3
Steven Degutis <sbdegutis@gmail.com>
2025-07-26 08:24:30 -0500

    file

--- my_main.c
+++ my_main.c
@@ -18,6 +18,7 @@ static void checkLicense();
 static void loadUserCodeFromDisk();
 static void callsig(enum asmevent ev, UINT32 arg);
 
+HANDLE file;
 static int running = 1;
 
 enum asmevent {
@@ -99,23 +100,43 @@ static void loadUserCodeFromDisk() {
 	SHGetKnownFolderPath(&FOLDERID_RoamingAppData, 0, NULL, &buf);
 	StrCpyW(userfile, buf);
 	CoTaskMemFree(buf);
-
 	PathAppendW(userfile, L"hram\\hsig.s");
 
-	//HANDLE file = CreateFileW(userfile, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	//printf("file? %d\n", file == INVALID_HANDLE_VALUE);
-	//printf("file? %d\n", CloseHandle(file));
-
-	//printf("exist = %d\n", PathFileExistsW(userfile));
-	//printf("path = %ls\n", userfile);
 
 
-	//WideCharToMultiByte(CP_UTF8, 0, wpath, -1, userdir, MAX_PATH, NULL, NULL);
+	file = CreateFileW(userfile, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
+	if (GetLastError() != ERROR_ALREADY_EXISTS) {
+		static const char src[] =
+			"mov rax, rcx        ; copy event arg to rax\n"
+			"mov [0x30100], rax  ; copy rax to screen pixel 1\n"
+			"\n"
+			"mov rax, [0x30006]\n"
+			"mov [0x30109], rax\n"
+			"\n"
+			"sub rsp, 24\n"
+			"call [0x30030]\n"
+			"add rsp, 24\n"
+			"\n"
+			"ret\n";
 
+		DWORD written;
+		BOOL ok = WriteFile(file, src, sizeof(src) - 1, &written, NULL);
+		printf("ok: %d\n", ok);
+		printf("written: %d\n", written);
+		StringCchCopyA(usersrc, sizeof(src), src);
+	}
+	else {
+		DWORD read;
+		BOOL ok = ReadFile(file, usersrc, 0x2000, &read, NULL);
+		printf("ok: %d\n", ok);
+		printf("read: %d\n", read);
+	}
 
-
-	//printf("%s\n", userdir);
+	printf("SOURCE\n");
+	printf("======\n");
+	printf("%s", usersrc);
+	printf("======\n");
 }
 
 
